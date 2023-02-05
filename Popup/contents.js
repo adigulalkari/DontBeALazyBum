44676b296d16adae72b9a021dc98a053d7adc125
adigulalkari <adigulalkari825@gmail.com>
2023-02-05 15:15:54 +0530

    added popup

---
+++ Popup/contents.js
@@ -0,0 +1,23 @@
+
+const delay_popup = 10*1000;
+let timer
+const alertBtn  = document.getElementById('sweetalert')
+
+function start(){
+    timer = setInterval(function()
+    {alert("KEEP your back straight!!!")},
+    delay_popup);
+}
+function stop(){
+    clearInterval(timer)
+}
+
+    alertBtn.addEventListener('click', function(){
+        chrome.tabs.query({active:true, currentWindow: true}, function(tabs){
+            start()
+        })
+    })
+    alertBtn.addEventListener('dblclick', function(){
+        this.style.backgroundColor= 'red'
+        stop()
+    })
